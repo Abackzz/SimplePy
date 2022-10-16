ac4e138764736142329fad5e7d9dae64af9b0191
Abhishek Singh <115918514+Abackzz@users.noreply.github.com>
2022-10-16 14:17:48 +0530

    Update file1.py

--- file1.py
+++ file1.py
@@ -1,9 +1,9 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        curr = nums_index[begin][0] + nums_index[end][0]
-        if curr == target:
-            return [nums_index[begin][1], nums_index[end][1]]
-        elif curr < target:
+class Answer(object):
+    def SumofTwo(self, nums, target):
+        current = main_index[begin][0] + main_index[end][0]
+        if current == target:
+            return [main_index[begin][1], main_index[end][1]]
+        elif current < target:
             begin += 1
         else:
             end -= 1
@@ -11,5 +11,5 @@ class Solution(object):
 
 if __name__ == '__main__':
     # begin
-    s = Solution()
-    print s.twoSum([3, 2, 4], 6)
+    s = Answer()
+    print s.SumofTwo([1, 3, 5], 7)
